bdb02f1a8a377d44c99027a448fdf4c925d00587
Steve Womble <swomble@aol.com>
2020-04-23 02:11:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
-
+def my_collect(array)
+ x = 0
+   students = []
+    while x < array.length 
+        x += 1
+        my_collect(languages) do |language|
+            name.split(" ").first
+          end
+          my_collect(students) do |student|
+            lang.upcase
+          end
+        array = languages + students << yield(array[i])
+          end
+    array
+end
