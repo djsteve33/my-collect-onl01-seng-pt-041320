707eb079be3f048284590dba4b7afa6358ceab29
Steve Womble <swomble@aol.com>
2020-04-23 03:25:36 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+ x = 0
+   new_array = []
+    while x < array.length 
+        new_array << yield(array[x])
+        x += 1
+     end
+    new_array
+end
 
 
+#def my_collect(array)
+  #make a new array
+  #loop through each item of the array
+   #apply each item to the block of code block_given?
+   #add each new item to a new array
+  #return the array of new itmes 
+#end
